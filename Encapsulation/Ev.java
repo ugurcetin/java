da0c8ac0f9a1c320369cb747bb6a707eca44d87f
Uğur Çetin <ugurcetin41@gmail.com>
2017-10-12 14:34:02 +0300

    Encapsulation

---
+++ Encapsulation/Ev.java
@@ -0,0 +1,13 @@
+package encapsulation;
+
+/**
+ * Created by UGURCETIN on 29.10.2016.
+ */
+public class Ev {
+
+    protected void bilgisayar(){
+        System.out.println("Bilgisayar metodu");
+    }
+
+    protected String fatura = "Faturalar";
+}
