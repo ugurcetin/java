6cdff1a0339eca3fd722fb2a6e56fe2cb39b643a
Uğur Çetin <ugurcetin41@gmail.com>
2017-10-12 14:36:36 +0300

    Loops

---
+++ Loops/While.java
@@ -0,0 +1,17 @@
+package Donguler;
+
+/**
+ * Created by UGURCETIN on 20.06.2017.
+ */
+public class While {
+    public static void main(String[] args) {
+        int sayi = 1;
+
+        while (sayi < 5) {
+            System.out.println("Hello" + "\n");
+            sayi++;
+        }
+
+    }
+}
+
