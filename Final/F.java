537633159e1472c94e3bc6335a9d9b73df6c9c08
Uğur Çetin <ugurcetin41@gmail.com>
2017-10-12 14:35:13 +0300

    Final

---
+++ Final/F.java
@@ -0,0 +1,26 @@
+package Final;
+
+/**
+ * Created by UGURCETIN on 17.07.2017.
+ */
+public class F {
+
+    //Final anahtar sözcüğü ile sabit değişken tanımlanabilir.
+
+    final double PISAYISI = 3.14;
+
+    //Final sözcüğü varsa kalıtım alınamaz.
+
+
+    //public final class A{
+    //
+    // }
+
+    //B extends A {
+    //
+    //
+    // }
+
+    //Final sözcüğü bir METODUN başında varsa OVERRİDE edilemez.
+
+}
